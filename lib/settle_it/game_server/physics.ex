a8812ab31e305ca6b8ddc707b15fc9e816e2772e
John Hunter Bowen <johnhunterbowen@gmail.com>
2024-11-13 06:49:14 -0500

    skip compile if cargo not present

--- lib/settle_it/game_server/physics.ex
+++ lib/settle_it/game_server/physics.ex
@@ -1,5 +1,10 @@
 defmodule SettleIt.GameServer.Physics do
+  @skip_compilation? is_nil(System.find_executable("cargo"))
   @mode if Mix.env() == :prod || System.get_env("RUST_ENV") == "prod", do: :release, else: :debug
 
-  use Rustler, otp_app: :settle_it, crate: :physics, mode: @mode
+  use Rustler,
+    otp_app: :settle_it,
+    crate: :physics,
+    mode: @mode,
+    skip_compilation?: @skip_compilation?
 end
